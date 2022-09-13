3fb2e545f13327cd4bf4015412b7def262a4f888
iravishah <ravi.shah@contentstack.com>
2022-09-13 19:41:11 +0530

    Skelton | Integrated config module to access environment variables in the application and added class validator module for the validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { loadConfig } from './framework/module.options';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(loadConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
