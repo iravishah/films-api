6848c5b908299a216d7a1e2d4044e95d1c93b123
iravishah <ravi.shah@contentstack.com>
2022-09-13 19:52:40 +0530

    Ecosystem | Integrated mongoose ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { loadConfig } from './framework/module.options';
+import { MongooseConfigService } from './framework/mongoose-config.service';
 
 @Module({
   imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: MongooseConfigService
+    }),
     ConfigModule.forRoot(loadConfig()),
   ],
   controllers: [AppController],
--- src/framework/module.options.ts
+++ src/framework/module.options.ts
@@ -1,10 +1,11 @@
 import { ConfigModuleOptions } from "@nestjs/config";
 import * as path from "path";
+import { mongooseConfig } from "./mognoose.config";
 
 export function loadConfig(): ConfigModuleOptions {
     return {
         envFilePath: path.join(__dirname, '../../', (process.env.NODE_ENV || 'development') + '.env'),
-        load: [],
+        load: [mongooseConfig],
         expandVariables: true
     }
 }
