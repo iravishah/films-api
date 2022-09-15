64bf1d95af15ddfd36debe675fc6827923f59d9d
iravishah <ravi.shah@contentstack.com>
2022-09-15 13:37:59 +0530

    Swagger | Added swagger documentation for the apis

--- src/app/comment/dto/create-comment.dto.ts
+++ src/app/comment/dto/create-comment.dto.ts
@@ -1,12 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateCommentDto {
     @IsNotEmpty()
     @IsString()
+    @ApiProperty({
+        description: 'Film for which you have to add the comment'
+    })
     film_id: string;
 
     @IsNotEmpty()
     @IsString()
+    @ApiProperty({
+        description: 'Comment for the film'
+    })
     comment: string;
 
     @IsOptional()
